9304b27d1113fa6e4c37a08f740cc1edc4576c60
Bhasvanth Dev Vajrapu Venkata <21btrcm020@jainuniversity.ac.in>
2024-07-15 21:56:04 +0530

    commit

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,114 @@
+import 'dart:convert';
+
+import 'package:ecommerce_app/constants/error_handling.dart';
+import 'package:ecommerce_app/constants/utils.dart';
+import 'package:ecommerce_app/features/home/screens/home_screen.dart';
+import 'package:ecommerce_app/models/user.dart';
+import 'package:ecommerce_app/providers/user_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../constants/global_variables.dart';
+
+class AuthService {
+  //signup user
+
+  void signUpUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      User user = User(
+          id: '',
+          name: name,
+          password: password,
+          address: '',
+          type: '',
+          token: '',
+          email: email);
+      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          });
+      print(res.statusCode);
+      httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () {
+            showSnackBar(
+                context, 'Account created! Login with the same credentials!');
+          });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  //signIn
+  void signInUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      http.Response res = await http.post(Uri.parse('$uri/api/signin'),
+          body: jsonEncode({
+            'email': email,
+            'password': password,
+          }),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          });
+      print(res.body);
+      httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () async {
+            Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+            Navigator.pushNamedAndRemoveUntil(
+                context, HomeScreen.routeName, (route) => false);
+          });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  //get User data
+  void getUserData(
+    BuildContext context,
+  ) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+      var tokenRes = await http.post(Uri.parse('$uri/tokenIsValid'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'x-auth-token': token!
+          });
+
+      var response = jsonDecode(tokenRes.body);
+      if (response == true) {
+        http.Response userRes = await http.get(Uri.parse('$uri/'),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+              'x-auth-token': token
+            });
+        var userProvider =  Provider.of<UserProvider>(context,listen: false);
+        userProvider.setUser(userRes.body);
+      }
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:ecommerce_app/constants/global_variables.dart';
+import 'package:ecommerce_app/features/auth/screens/auth_screen.dart';
+import 'package:ecommerce_app/features/auth/services/auth_service.dart';
+import 'package:ecommerce_app/features/home/screens/home_screen.dart';
+import 'package:ecommerce_app/providers/user_provider.dart';
+import 'package:ecommerce_app/router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'common/widgets/bottom_bar.dart';
+
+void main() {
+  runApp(MultiProvider(providers:[
+    ChangeNotifierProvider(create: (context)=> UserProvider())
+  ],child: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  final AuthService authService = AuthService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    authService.getUserData(context);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+        colorScheme: ColorScheme.light(primary: GlobalVariables.secondaryColor),
+        useMaterial3: false,
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty?const BottomBar():const AuthScreen(),
+    );
+  }
+}
